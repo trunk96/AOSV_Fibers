c39944f62fb4f33cb7b808f399a33dd7f8f5ea06
trunk96 <trunk96@gmail.com>
2018-09-08 01:10:33 +0200

    moved some code from ioctl.c to ioctl.h

--- module/ioctl.c
+++ module/ioctl.c
@@ -5,22 +5,7 @@
 #include "ioctl.h"
 
 
-
-#define IOCTL_CONVERT_THREAD_TO_FIBER _IO(major, 0)
-#define IOCTL_CREATE_FIBER _IOW(major, 1, void*)
-#define IOCTL_SWITCH_TO_FIBER _IOW(major, 2, void*)
-#define IOCTL_FLS_ALLOC _IO(major, 3)
-#define IOCTL_FLS_FREE _IOW(major, 4, void*)
-#define IOCTL_FLS_GETVALUE _IOWR(major, 5, void*) //we use void* since ve have to write the index to the kernel
-                                                  //and we have to read the result into an user buffer
-#define IOCTL_FLS_SETVALUE _IOW(major, 6, void*)  //we use void* to pack both the index and the buffer to be passed to the kernel
-
-
-#define MESSAGE_MAX_LEN 256
-
 char string_message[MESSAGE_MAX_LEN] = "";
-
-
 
 void publish_ioctl_message ()
 {
--- module/ioctl.h
+++ module/ioctl.h
@@ -1,3 +1,16 @@
+#define IOCTL_CONVERT_THREAD_TO_FIBER _IO(major, 0)
+#define IOCTL_CREATE_FIBER _IOW(major, 1, void*)
+#define IOCTL_SWITCH_TO_FIBER _IOW(major, 2, void*)
+#define IOCTL_FLS_ALLOC _IO(major, 3)
+#define IOCTL_FLS_FREE _IOW(major, 4, void*)
+#define IOCTL_FLS_GETVALUE _IOWR(major, 5, void*) //we use void* since ve have to write the index to the kernel
+                                                  //and we have to read the result into an user buffer
+#define IOCTL_FLS_SETVALUE _IOW(major, 6, void*)  //we use void* to pack both the index and the buffer to be passed to the kernel
+
+
+#define MESSAGE_MAX_LEN 256
+
+
 extern long major;
 void publish_ioctl_message (void);
 static long fibers_ioctl (struct file *, unsigned int, unsigned long);

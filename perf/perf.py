a860fd11fea39b07507f1d16c08093dbd0ecbea5
trunk96 <trunk96@gmail.com>
2018-10-09 22:30:34 +0200

    maybe finished

--- perf/perf.py
+++ perf/perf.py
@@ -8,14 +8,14 @@ if len(sys.argv) > 1 and sys.argv[1] == "-exec":
     s = list()
 
     for i in range(0, 15):
-        result = subprocess.run(['../benchmark/2018-fibers/test', '550'], stdout = subprocess.PIPE)
+        result = subprocess.run(['../benchmark/2018-fibers/test', str((i+1)*32)], stdout = subprocess.PIPE)
         a = result.stdout.decode('utf-8').split('\n')[2];
         s.append(a)
         print(a)
 
     f = open("work_time.txt", "w");
     for str in s:
-        f.write(str)
+        f.write(str+"\n")
     f.close()
 
 elif len(sys.argv) > 1 and sys.argv[1] == "-execusr":
@@ -23,27 +23,35 @@ elif len(sys.argv) > 1 and sys.argv[1] == "-execusr":
     s = list()
 
     for i in range(0, 15):
-        result = subprocess.run(['../benchmark/2018-fibers/test', '550'], stdout = subprocess.PIPE)
+        result = subprocess.run(['../benchmark/2018-fibers/test', str((i+1)*32)], stdout = subprocess.PIPE)
         a = result.stdout.decode('utf-8').split('\n')[2];
         s.append(a)
         print(a)
 
     f = open("work_time_user.txt", "w");
     for str in s:
-        f.write(str)
+        f.write(str+"\n")
     f.close()
 
 else:
     f1 = open("work_time.txt", "r");
     f2 = open("work_time_user.txt", "r");
+    f3 = open("work_time_550.txt", "r");
+    f4 = open("work_time_user_550.txt", "r")
 
     l1 = list()
     l2 = list()
+    l3 = list()
+    l4 = list()
 
     for line in f1:
         l1.append(float(line.split(':')[1].strip()));
-    for line2 in f2:
-        l2.append(float(line2.split(':')[1].strip()));
+    for line in f2:
+        l2.append(float(line.split(':')[1].strip()));
+    for line in f3:
+        l3.append(float(line.split(':')[1].strip()));
+    for line in f4:
+        l4.append(float(line.split(':')[1].strip()));
 
     plt.xlabel("Iteration")
     plt.ylabel("Time (per-fiber)")
@@ -53,7 +61,7 @@ else:
     plt.legend()
     plt.show()
     plt.ylabel("Time (per-fiber)")
-    plt.boxplot([l1, l2], showmeans=True)
+    plt.boxplot([l3, l4], showmeans=True)
     x_axis = np.arange(1, 3, 1)
     plt.xticks(x_axis, ["kernel", "user"])
 
